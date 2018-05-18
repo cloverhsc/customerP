518d5d0bb8f1830563999f1873ab3db3fb0c59b3
clover <clover.hsc@gmail.com>
2018-05-18 14:01:48 +0800

    change style of login.css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ErrorStateMatcher } from '@angular/material';
-import {
-  FormGroup, FormControl,
-  FormGroupDirective, NgForm,
-  Validators, ValidatorFn
-  } from '@angular/forms';
-
-
-export class EarlyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl |
-      null, form: FormGroupDirective |
-        NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && control.dirty);
-  }
-}
-
-export class SurveryComponent {
-  surveyForm: FormGroup;
-  EarlyErrorStateMatcher = new EarlyErrorStateMatcher();
-}
 
 @Component({
   selector: 'app-login',
@@ -31,8 +10,7 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   onSubmit() {
     console.log('click submit!');
